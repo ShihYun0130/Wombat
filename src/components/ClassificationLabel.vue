9f7b78bbcd6dc5f87a030c89e49028c45f3c53f4
Frank Chen <biohazard93011@ntu.im>
2020-10-27 11:55:17 +0800

    [feature][Classification Layout] add imageCard

--- src/components/ClassificationLabel.vue
+++ src/components/ClassificationLabel.vue
@@ -1,60 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="classification">
+    <div class="header">
+      <i>
+        <img class="small-icon" src="../assets/icons/compare-24px.svg">
+      </i>
+      <span>
+        <h1 class="taskName">
+          {{ taskName }}
+        </h1>
+      </span>
+    </div>
+
+    <h3>
+        請問下列圖片哪些屬於
+        <span style="color:rgb(0,195,0)">{{selectedClass}}</span>
+        <span>?</span>
+    </h3>
+    <ImageCard :imgList="imgList" :rows="2"/>
+
+
   </div>
 </template>
 
 <script>
+import ImageCard from './ImageCard.vue'
+// import axios from "axios"
+
 export default {
   name: 'Classification',
+  components: {
+    ImageCard
+  },
   props: {
     msg: String,
-    TaskName: String,
-
+    taskName: String,
+  },
+  data: function(){
+    return{
+        imgList:[
+            {path:require('../assets/img/dog.png')},
+            {path:require('../assets/img/cat.png')},
+        ],
+        selectedClass:'狗',
+        rows:2,
+    }
+  },
+  mounted() {
+    // this.selectedClass = "狗";
+    // axios
+    //   .get('https://www.runoob.com/try/ajax/json_demo.json')
+    //   .then(response => (this.SelectedClass = response))
+    //   .catch(function (error) { 
+    //     console.log(error);
+    //   });
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .small-icon {
+    width: 14pt;
+    margin-right: 4px;
+  }
+  .taskName {
+    font-size: 15pt;
+    margin-top:8px;
+  }
+  h3 {
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
 </style>
---
+++ src/components/ImageCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="imageCard">
+    <div v-for="i in rows" :key="i" class="row">
+        <div class="container">
+            <div v-for="item in imgList" :key="item.path">
+            <img class="label_img" :src="item.path" alt="Card image cap">
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from "axios"
+export default {
+  name: 'ImageCard',
+  props: {
+    imgList: Array,
+    rows: Number,
+  },
+  mounted() {
+    // axios
+    //   .get('https://www.runoob.com/try/ajax/json_demo.json')
+    //   .then(response => (this.SelectedClass = response))
+    //   .catch(function (error) { 
+    //     console.log(error);
+    //   });
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row{
+  margin-bottom: 10px;
+}
+.label_img {
+  border-radius: 10px;
+}
+.container{
+  display: flex;
+  justify-content: center;
+  width: 70%;
+  /* height: 150px; */
+  margin: auto;
+}
+.container div{
+  display: flex;
+  margin-right: 10px;
+}
+.container img{
+  height: 100%;
+  width: 100%;
+}
+</style>
